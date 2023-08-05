50cc04b55d861548377ca4d0c6f7a3e0972aaaa1
unknown <thakurusama72@gmail.com>
2023-08-05 17:25:25 +0530

    frontend + backend integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  pages:true,
+  pages: true,
   modules: [
-    'nuxt-icon',
-    '@nuxtjs/tailwindcss',
-    //'@nuxtjs/supabase',
-    '@pinia/nuxt',
-    "@vite-pwa/nuxt",
-],
-runtimeConfig: {
-  public: {
-    bucketUrl: process.env.BUCKET_URL
+      'nuxt-icon',
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/supabase',
+      '@pinia/nuxt',
+      "@vite-pwa/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL
+    }
+  },
+  devtools: { enabled: false },
+  pwa: {
+    manifest: {
+      name: "Famy",
+      short_name: "Famy",
+      description: "Famy A Place to Talk it Out",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    }
   }
-},
-  devtools: { enabled: true }
 })
